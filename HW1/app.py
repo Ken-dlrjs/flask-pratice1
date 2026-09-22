45fa0d0d743d9bdff4f18a7eadb6646487802496
이건 <ken1180@sju.ac.kr>
2026-09-22 18:55:44 +0900

    feat: HW 취미 페이지 구현

--- HW1/app.py
+++ HW1/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def indentifier():
     return render_template("indentifier.html")
 
+@app.route("/profile")
+def profile():
+    mhobbies = ["야구", "여행", "Formula 1"]
+    return render_template("profile.html", hobbies = mhobbies)
+
 if __name__ == "__main__":
     app.run(debug = True)
